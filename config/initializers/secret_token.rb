ebe0852a6b47d4bff9fc3efd2dfc928aca2b1de1
vagrant <dinhhoanglong91@gmail.com>
2013-10-30 14:08:32 +0000

    Generate a new Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HotPans::Application.config.secret_key_base = '01c40ef5f9eedc5e130750216d9bb05b8dc0bc3fc34206a06b2d1e6b41e000d28085b5f887bf8fa4e4b63f320103e97ddaedca2f195583a863dc8e03d1949dac'
