1cd96b1cd84bcc999c320818df44d36cdff4df43
hoanglong <dinh.hoang.long.245@donuts.ne.jp>
2013-11-25 05:59:57 +0900

    English text -> japanese

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -7,7 +7,7 @@ class AuthenticationsController < ApplicationController
 
     if authentication
       # Authentication found, sign the bread_store_manager in.
-      flash[:notice] = "Signed in successfully."
+      flash[:notice] = "ログインしました。"
       sign_in(authentication.bread_store_manager)
       redirect_to bread_store_managers_url
     else
@@ -15,10 +15,10 @@ class AuthenticationsController < ApplicationController
       bread_store_manager = BreadStoreManager.new
       bread_store_manager.apply_omniauth(auth)
       if bread_store_manager.save(:validate => false)
-        flash[:notice] = "Account created and signed in successfully."
+        flash[:notice] = "新規ユーザーを登録しました。"
         sign_in_and_redirect(:bread_store_manager, bread_store_manager)
       else
-        flash[:error] = "Error while creating a bread_store_manager account. Please try again."
+        flash[:error] = "エラーが発生しました。"
         redirect_to root_url
       end
     end
