daded4811c2132f7d651b365648e694710d67040
gonewiththewind1312 <dinhhoanglong91@gmail.com>
2013-12-11 03:58:54 +0900

    Fix form legend

--- app/helpers/bread_stores_helper.rb
+++ app/helpers/bread_stores_helper.rb
@@ -10,6 +10,15 @@ module BreadStoresHelper
     end
   end
   
+  def getFormLegendByAction
+    current_action = params[:action]
+    if (current_action == "new")
+      "パン屋登録"
+    elsif (current_action == "edit")
+      "パン屋情報編集"
+    end
+  end
+  
   def getFormActionURL(current_view)
     case current_view
     when "new"
