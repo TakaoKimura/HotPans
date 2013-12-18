9bf910b54e33d8cec13ea988834041c0690c6c8a
gonewiththewind1312 <dinhhoanglong91@gmail.com>
2013-12-19 00:33:30 +0900

    Fix text

--- app/helpers/breads_helper.rb
+++ app/helpers/breads_helper.rb
@@ -2,33 +2,33 @@ module BreadsHelper
   def getButtonNameByAction
     current_action = params[:action]
     if (current_action == "new")
-      "??"
+      "作成"
     elsif (current_action == "edit")
-      "??"
+      "更新"
     else
-      "??"
+      "確定"
     end
   end
   
   def getFormLegendByAction
     current_action = params[:action]
     if (current_action == "new")
-      "?????"
+      "パン屋登録"
     elsif (current_action == "edit")
-      "???????"
+      "パン屋情報編集"
     end
   end
   
   def getFormActionURL(current_view)
     case current_view
     when "new"
-      confirm_bread_path(@bread)
+      confirm_bread_store_path(@bread_store)
     when "confirm"
-      bread_path(@bread)
+      bread_store_path(@bread_store)
     when "edit"
-      edit_confirm_bread_path(@bread)
+      edit_confirm_bread_store_path(@bread_store)
     when "edit_confirm"
-      bread_path(@bread)
+      bread_store_path(@bread_store)
     end
   end
   
