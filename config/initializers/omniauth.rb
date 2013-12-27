38455bf9a7bc2bd41c6f14b10b54b370cf479e0f
gonewiththewind1312 <dinhhoanglong91@gmail.com>
2013-12-27 17:57:16 +0000

    Config for login facebook from heroku and local

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,17 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # The following is for facebook
-  provider :facebook, "559426714147437", "1f66f08a6957b872de20a37995bd1410", {:scope => 'email, read_stream, read_friendlists, friends_likes, friends_status, offline_access'}
-  #provider :facebook, [559426714147437], ["1f66f08a6957b872de20a37995bd1410"]
+  
+  if Rails.env.development?
+    # Development Enviroment. url: localhost:3000
+    app_id = "481775351940477"
+    app_secret = "d74e5702ec84758741a38463ddf50671"
+  else
+    # Production Enviroment. url: hotpans.herokuapp.com
+    app_id = "559426714147437"
+    app_secret = "1f66f08a6957b872de20a37995bd1410"
+  end
+  
+  provider :facebook, app_id, app_secret, {:scope => 'email, read_stream, read_friendlists, friends_likes, friends_status, offline_access'}
  
   # If you want to also configure for additional login services, they would be configured here.
 end
