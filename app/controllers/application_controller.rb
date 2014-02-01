f42b62a69932f59650a2f20fd2939e4aac73ca31
gonewiththewind1312 <dinhhoanglong91@gmail.com>
2014-02-01 19:38:16 +0000

    Force reload page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :set_cache_buster
   def require_login
     unless current_bread_store_manager
       redirect_to bread_stores_url
     end
+  end
+  def set_cache_buster
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
   end
 end
